bd653f9ff2d7f1f6050e033dccd38fa61365c996
Jehad Abusir <jabusir@gmail.com>
2021-09-05 21:05:39 -0700

    Remove dogshit shipping API

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,7 +1,7 @@
 "use strict";
 require("dotenv").config();
 const Easypost = require("@easypost/api");
-const api = new Easypost(process.env.EASYPOST_KEY);
+// const api = new Easypost(process.env.EASYPOST_KEY);
 
 module.exports = {
   create: async (ctx) => {
@@ -15,108 +15,108 @@ module.exports = {
       email,
     } = ctx.request.body;
 
-    const createShipments = async (to, from, parcels) => {
-      const shipments = parcels.map((p) => {
-        const shipment = apiMakeShipment({
-          to_address: to,
-          from_address: from,
-          parcel: p,
-        });
-        return shipment;
-      });
-      return Promise.all(shipments);
-    };
+    // const createShipments = async (to, from, parcels) => {
+    //   const shipments = parcels.map((p) => {
+    //     const shipment = apiMakeShipment({
+    //       to_address: to,
+    //       from_address: from,
+    //       parcel: p,
+    //     });
+    //     return shipment;
+    //   });
+    //   return Promise.all(shipments);
+    // };
 
     const event = ctx.request.body;
-    const getDimensions = async (name) => {
-      try {
-        const product = await strapi.services.product.findOne({
-          Name: name,
-        });
-        const { id, Name, Height, Length, Width, Weight } = product;
-        return { id, Name, Height, Length, Width, Weight };
-      } catch (e) {
-        return e;
-      }
-    };
+    // const getDimensions = async (name) => {
+    //   try {
+    //     const product = await strapi.services.product.findOne({
+    //       Name: name,
+    //     });
+    //     const { id, Name, Height, Length, Width, Weight } = product;
+    //     return { id, Name, Height, Length, Width, Weight };
+    //   } catch (e) {
+    //     return e;
+    //   }
+    // };
 
-    const apiMakeParcel = async (parcel) => {
-      const apiParcel = new api.Parcel(parcel);
-      return apiParcel.save();
-    };
+    // const apiMakeParcel = async (parcel) => {
+    //   const apiParcel = new api.Parcel(parcel);
+    //   return apiParcel.save();
+    // };
 
-    const apiMakeShipment = async (shipment) => {
-      const apiShipment = new api.Shipment(shipment);
-      return apiShipment.save();
-    };
+    // const apiMakeShipment = async (shipment) => {
+    //   const apiShipment = new api.Shipment(shipment);
+    //   return apiShipment.save();
+    // };
 
-    const createParcels = async (productsWQt) => {
-      const dimensions = productsWQt.map((prod) => getDimensions(prod.name));
-      try {
-        const parcels = await Promise.all(dimensions);
-        const parcelsArr = parcels.map((p) => {
-          const parcel = apiMakeParcel({
-            length: p.Length,
-            width: p.Width,
-            height: p.Height,
-            weight: p.Weight,
-          });
-          return parcel;
-        });
-        return Promise.all(parcelsArr);
-      } catch (e) {
-        return e;
-      }
-    };
+    // const createParcels = async (productsWQt) => {
+    //   const dimensions = productsWQt.map((prod) => getDimensions(prod.name));
+    //   try {
+    //     const parcels = await Promise.all(dimensions);
+    //     const parcelsArr = parcels.map((p) => {
+    //       const parcel = apiMakeParcel({
+    //         length: p.Length,
+    //         width: p.Width,
+    //         height: p.Height,
+    //         weight: p.Weight,
+    //       });
+    //       return parcel;
+    //     });
+    //     return Promise.all(parcelsArr);
+    //   } catch (e) {
+    //     return e;
+    //   }
+    // };
 
     switch (event.type) {
       case "charge.succeeded":
         const paymentIntent = event.data.object;
         const { shipping, receipt_email, metadata } = paymentIntent;
-        const { address } = shipping.address;
+        const { address } = shipping;
         try {
-          const fromAddress = new api.Address({
-            company: "Bands Unlimited LLC.",
-            street1: "13512 Dean St",
-            city: "Tustin",
-            state: "CA",
-            zip: "92780",
-            phone: "5623707369",
-          });
+          // const fromAddress = new api.Address({
+          //   company: "Bands Unlimited LLC.",
+          //   street1: "13512 Dean St",
+          //   city: "Tustin",
+          //   state: "CA",
+          //   zip: "92780",
+          //   phone: "5623707369",
+          // });
 
-          const parcelsPayload = createParcels(
-            Object.entries(metadata).map(([key, value]) => ({
-              name: key,
-              quantity: value,
-            }))
-          );
+          // const parcelsPayload = createParcels(
+          //   Object.entries(metadata).map(([key, value]) => ({
+          //     name: key,
+          //     quantity: value,
+          //   }))
+          // );
 
-          const toAddress = new api.Address({
-            name: shipping.name,
-            street1: address.line1,
-            street2: address.line2,
-            city: address.city,
-            state: address.state,
-            zip: address.postal_code,
-          });
+          // const toAddress = new api.Address({
+          //   name: shipping.name,
+          //   street1: address.line1,
+          //   street2: address.line2,
+          //   city: address.city,
+          //   state: address.state,
+          //   zip: address.postal_code,
+          // });
 
-          await fromAddress.save();
+          // await fromAddress.save();
 
-          await toAddress.save();
+          // await toAddress.save();
 
-          const parcels = await Promise.resolve(parcelsPayload);
+          // const parcels = await Promise.resolve(parcelsPayload);
 
-          const shipmentsPayload = await createShipments(
-            toAddress,
-            fromAddress,
-            parcels
-          );
+          // const shipmentsPayload = await createShipments(
+          //   toAddress,
+          //   fromAddress,
+          //   parcels
+          // );
 
-          const shipments = await Promise.all(shipmentsPayload);
+          // const shipments = await Promise.all(shipmentsPayload);
 
-          const labelsRates = shipments.map((ship) => {
-            return ship.buy(ship.lowestRate(["USPS"], ["First"]));
-          });
+          // const labelsRates = shipments.map((ship) => {
+          //   return ship.buy(ship.lowestRate(["USPS"], ["First"]));
+          // });
 
           const labels = await Promise.all(labelsRates);
           const order = await strapi.services.order.create({
